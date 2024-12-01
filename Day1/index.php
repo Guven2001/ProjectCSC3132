a249b36fb2055b1686668cb9540871753dd8a8fe
Guven2001 <139533207+Guven2001@users.noreply.github.com>
2024-12-01 13:16:59 +0000

    Create Table

--- Day1/index.php
+++ Day1/index.php
@@ -4,20 +4,37 @@ require_once 'dbconf.php';
 try{
 	//Query
 	$sql = "SELECT * FROM student";
-	//excute the query
+	//excute the quey
 	$result = mysqli_query($connect,$sql);
 	//check if data exists in the table
-	if (mysqli_num_rows($result)>0){
-		//fetch the data from rows
-		while ($row = mysqli_fetch_assoc($result)){
-			print_r($row);
+	if (mysqli_num_rows($result)>0) {
+		// fetch the data from rows
+		echo "<table border='1'>";
+		$col = mysqli_fetch_fields($result);
+		//print the colums
+		echo "<tr>";
+		foreach ($col as $value) {
+			//return an object
+			//print_r($value);
+			echo "<td>$value->name</td>";
 		}
-	}
-	else{
+		echo "</tr>";
+		
+		while ($row = mysqli_fetch_assoc($result)) {
+			//print the data in a table format
+			echo "<tr>";
+			foreach ($row as $key => $value) {
+				echo "<td>$value</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";
+	} 
+	else {
 		echo "No results";
 	}
+	
 }
-catch(Exception $e){
+ catch (Exception $e) {
 	die($e->getMessage());
 }
-?>

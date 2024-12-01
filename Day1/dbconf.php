6beaba13f32afe7f6411f177ceb964ca0a1fee67
Guven2001 <139533207+Guven2001@users.noreply.github.com>
2024-12-01 11:25:45 +0000

    Hide Password

--- Day1/dbconf.php
+++ Day1/dbconf.php
@@ -1,12 +1,17 @@
 <?php
 //constant variable
-define('SERVERNAME', '127.0.0.1:3306');
-define('USERNAME', 'root');
-define('PASSWORD', 'mariadb');
-define('DBNAME', 'school');
+//define('SERVERNAME', '127.0.0.1:3306');
+//define('USERNAME', 'root');
+//define('PASSWORD', 'mariadb');
+//define('DBNAME', 'school');
+$dbHost = getenv('DB_HOST');
+$dbName = getenv('DB_NAME');
+$dbUser = getenv('DB_USER');
+$dbPass = getenv('DB_PASS');
+
 try{
 	//connect with database
-	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+	$connect = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName);
 	if (!$connect){
 		die("Connection Failed".mysqli_connect_error());
 	}

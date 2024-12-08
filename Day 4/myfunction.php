eb513ddab3e9406bbce88cc3f0269ed7da4be0cf
Guven2001 <139533207+Guven2001@users.noreply.github.com>
2024-12-08 09:28:54 +0000

    Add GetStudentDetails function

--- Day 4/myfunction.php
+++ Day 4/myfunction.php
@@ -41,4 +41,50 @@ function PrintTable($tableName,$connect){
     //PrintTable("reviews",$connect);
     
     echo "............";
+
+    function GetStudentDetails($connect){
+        //query
+        try{
+        
+            $sql="select id,name from students  ";
+            
+        
+            //$sq1="SELECT * FROM $name";
+            
+        
+            //execute the query
+            $result=mysqli_query($connect,$sql);
+        
+            //check if data exists in the table
+            if(mysqli_num_rows($result)>0){
+                echo "<table border='1'>";
+                $col=mysqli_fetch_fields($result);
+                echo "<tr>";
+                foreach($col as $value){
+                    echo "<td>$value->name</td>";
+                }
+                echo "<td>View Student's Details</td>";
+                echo "</tr>";
+                
+
+               
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<tr>";
+                    foreach($row as $key=>$value){
+                        echo "<td>$value</td>";
+                    }
+                    $id=$row['id'];
+                    echo "<td><a href='StudentDetails.php?id=$id'>View</a></td>";
+                    echo "</tr>";
+                   
+                }
+                echo "</table>";
+            }else{
+                echo "No results";
+            }
+        }
+        catch(Exception $e){
+            die($e->getmessage());
+        }
+        }
     ?>
